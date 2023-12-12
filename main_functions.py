1f6e4c1b43bd8dde8542617ff99a40ef50f0e2d8
caoji <jintaoc2@illinois.edu>
2023-12-12 00:52:00 -0600

    add comments in main_funtion.py

--- main_functions.py
+++ main_functions.py
@@ -4,15 +4,16 @@ from matplotlib import pyplot as plt
 
 def ext_sel_da(df, country_code, start_year, end_year):
     """
-        Select data from a fixed Dataframe based on the given country abbreviation and time period (from start_year
-        to end_year) and form a dictionary with these data and their corresponding years. Finally, the dictionary
-        is returned.
+    Select data from a fixed Dataframe based on the given country abbreviation and time period (from start_year
+    to end_year) and form a dictionary with these data and their corresponding years. Finally, the dictionary
+    is returned.
 
-        param df: Dataframe
-        param country_code: Abbreviation of country name. For example, China is "CHN"
-        param start_year: First year of required information
-        param end_year: Last year of required information
-        return: A dictionary containing years and fertility rate data for the corresponding years
+    :param df: Dataframe
+    :param country_code: Abbreviation of country name. For example, China is "CHN"
+    :param start_year: First year of required information
+    :param end_year: Last year of required information
+    :return: A dictionary containing years and fertility rate data for the corresponding years
+
         >>> fr_data = pd.read_csv('Data/Fertility rates.csv', skiprows=4)
         >>> ext_sel_da(fr_data, 'CHN', 2018, 2021)
         {'2018': 1.554, '2019': 1.496, '2020': 1.281, '2021': 1.164}
@@ -26,13 +27,14 @@ def ext_sel_da(df, country_code, start_year, end_year):
 
 def ext_sel_covid(df, country_code):
     """
-        Find the required data from the Dataframe that records COVID-19 related data based on the given country
-        abbreviation and form a dictionary with these data and their corresponding years. Finally, the dictionary
-        is returned.
+    Find the required data from the Dataframe that records COVID-19 related data based on the given country
+    abbreviation and form a dictionary with these data and their corresponding years. Finally, the dictionary
+    is returned.
 
-        param df: Dataframe
-        param country_code: Abbreviation of country name. For example, China is "CHN"
-        return: A dictionary containing years and COVID-19 inflection data for the corresponding years
+    :param df: Dataframe
+    :param country_code: Abbreviation of country name. For example, China is "CN"
+    :return: A dictionary containing years and COVID-19 inflection data for the corresponding years
+
         >>> covid_data = pd.read_csv('Data/WHO-COVID-19-global-data.csv')
         >>> ext_sel_covid(covid_data, 'CN')
         Year
@@ -50,13 +52,14 @@ def ext_sel_covid(df, country_code):
 
 def ext_sel_polio(df, country_code):
     """
-            Find the required data from the Dataframe that records poliomyelitis related data based on the given country
-            abbreviation and form a dictionary with these data and their corresponding years. Finally, the dictionary
-            is returned.
+    Find the required data from the Dataframe that records poliomyelitis related data based on the given country
+    abbreviation and form a dictionary with these data and their corresponding years. Finally, the dictionary
+    is returned.
 
-            param df: Dataframe
-            param country_code: Abbreviation of country name. For example, China is "CHN"
-            return: A dictionary containing years and poliomyelitis inflection data for the corresponding years
+    :param df: Dataframe
+    :param country_code: Abbreviation of country name. For example, China is "CHN"
+    :return: A dictionary containing years and poliomyelitis inflection data for the corresponding years
+
             >>> polio_data = pd.read_csv('Data/the-number-of-reported-paralytic-polio-cases.csv')
             >>> ext_sel_polio(polio_data, 'CHN')
                   Year  Total (reported) polio cases
@@ -102,7 +105,7 @@ def ext_sel_polio(df, country_code):
             1465  2019                             1
             1466  2020                             0
             1467  2021                             0
-        """
+    """
     polio_data = df[df['Code'] == country_code]
     polio_cases = polio_data[['Year', 'Total (reported) polio cases']]
     return polio_cases
@@ -110,28 +113,30 @@ def ext_sel_polio(df, country_code):
 
 def ext_sel_hiv(df, country_code):
     """
-            Find the required data from the Dataframe that records COVID-19 related data based on the given country
-            abbreviation and form a dictionary with these data and their corresponding years. Finally, the dictionary
-            is returned.
+    Find the required data from the Dataframe that records HIV related data based on the given country
+    abbreviation and form a dictionary with these data and their corresponding years. Finally, the dictionary
+    is returned.
 
-            param df: Dataframe
-            param country_code: Abbreviation of country name. For example, China is "CHN"
-            return: A dictionary containing years and COVID-19 inflection data for the corresponding years
-            >>> covid_data = pd.read_csv('Data/WHO-COVID-19-global-data.csv')
-            >>> ext_sel_covid(covid_data, 'CN')
-            Year
-            2020       96673
-            2021       35398
-            2022    84792971
-            2023    14394816
-            Name: New_cases, dtype: int64
-        """
+    :param df: Dataframe
+    :param country_code: Abbreviation of country name. For example, China is "CHN"
+    :return: A dictionary containing years and HIV inflection data for the corresponding years
+    """
     hiv_data = df[df['Code'] == country_code]
     hiv_cases = hiv_data[['Year', 'Current number of cases of hiv/aids per 100 people, in both sexes aged 15-49 years']]
     return hiv_cases
 
 
 def plot_dt_draw(dt_dict, country_code, country, year_size):
+    """
+    Filter the corresponding information in the dictionary based on the abbreviation of the country name and
+    the complete name of the country to draw a line chart. The X-axis interval of the line chart is
+    year-size
+
+    :param dt_dict: Dictionary
+    :param country_code: Abbreviation of country name. For example, Syrian is "SYR"
+    :param country: Complete name of the country. For example, Syrian is "Syrian Arab Republic"
+    :param year_size: The X-axis interval of the line chart
+    """
     years = list(map(int, dt_dict.keys()))
     rates = list(dt_dict.values())
     plt.figure(figsize=(10, 6))
@@ -146,6 +151,17 @@ def plot_dt_draw(dt_dict, country_code, country, year_size):
 
 
 def calculate_change_rate(data_dict):
+    """
+    Calculate the annual change rate of the values in the dictionary value.
+
+    :param data_dict: A dictionary with year as key and data as value
+    :return: A dictionary with year as key and change rate as value
+
+    >>> fr_data = pd.read_csv('Data/Fertility rates.csv', skiprows=4)
+    >>> usa_fr = ext_sel_da(fr_data, 'USA', 2018, 2021)
+    >>> calculate_change_rate(usa_fr)
+    {'2019': -0.013587742122000621, '2020': -0.03780773739742087, '2021': 0.013706975327444389}
+    """
     change_rates = {}
     previous_value = None
     for year, value in data_dict.items():
@@ -166,6 +182,17 @@ def calculate_change_rate(data_dict):
 
 
 def plot_fertility_rates(data_dict, country_name, start_year, end_year, step=3):
+    """
+    Based on the country name and the selected time period (from start_year to end_year), draw a line chart from
+    the corresponding fertility rate information in the filter dictionary. The X-axis interval of the line chart
+    is step.
+
+    :param data_dict: Dictionary
+    :param country_name: Country name
+    :param start_year: First year of required information
+    :param end_year: Last year of required information
+    :param step: The X-axis interval of the line chart
+    """
     years = list(map(int, data_dict.keys()))
     rates = list(data_dict.values())
     plt.figure(figsize=(10, 6))
@@ -180,6 +207,12 @@ def plot_fertility_rates(data_dict, country_name, start_year, end_year, step=3):
 
 
 def plot_normalized_rates(fertility_rates, death_rates):
+    """
+    Draw a line chart based on data from the fertility dictionary and data from the mortality dictionary.
+
+    :param fertility_rates: Dictionary containing fertility data
+    :param death_rates: Dictionary containing fertility data
+    """
     df = pd.DataFrame({'Fertility_Rates': fertility_rates, 'Death_Rates': death_rates})
     df_normalized = (df - df.min()) / (df.max() - df.min())
     df_normalized.plot()
